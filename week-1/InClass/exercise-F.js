492f81802ea082dfaf2e1530439a3ad34152dc97
Laura Beleño <vaneessa10@gmail.com>
2022-08-19 21:47:18 +0200

    cambio ejerciciof

--- week-1/InClass/exercise-F.js
+++ week-1/InClass/exercise-F.js
@@ -1,8 +1,11 @@
 const number0fStudents= 150
 const number0fMentors= 16
-
 const totalStudentsAndMentors= number0fStudents + number0fMentors
 
-console.log ( Math.round ((100* number0fStudents)/totalStudentsAndMentors)+ "%")
-console.log (Math.round((100*number0fMentors)/totalStudentsAndMentors )+ "%")
+const greeting=number0fStudents
+const greeting2=number0fMentors
+const greeting3=totalStudentsAndMentors
+
+console.log( Math.round ((100* number0fStudents)/totalStudentsAndMentors)+ "%  of students")
+console.log(Math.round((100*number0fMentors)/totalStudentsAndMentors )+ "%  of mentors")
 
