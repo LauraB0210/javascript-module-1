8b234512df22c703beb73a85b2b5337187493b25
Laura Beleño <vaneessa10@gmail.com>
2022-08-20 12:32:49 +0200

    ejercicio H

---
+++ week-1/InClass/exercise-H.js
@@ -0,0 +1,10 @@
+function sum(a, b) {
+    const sumResult = a + b;
+    return "El total de estudiantes y mentores es: " + sumResult;
+}
+
+const result = sum(150, 16);
+console.log(result);
+
+
+  
