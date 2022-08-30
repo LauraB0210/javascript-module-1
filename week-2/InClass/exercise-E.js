3d9138106c7e452cdf3ab1497eda1eaed9293bf3
Laura Beleño <vaneessa10@gmail.com>
2022-08-30 17:13:33 +0200

    ejercicio E

---
+++ week-2/InClass/exercise-E.js
@@ -0,0 +1,16 @@
+let num = 10
+//estoy guardando un valor en una variable (undefined)
+num > 5 && num < 15
+//Expresion que retorna true
+num < 10 || num === 10
+//expresion que retorna true
+false || true
+//expresion que retorna true
+!true
+//expresion que retorna false
+let greaterThan5 = num > 5
+//estoy guardando un valor en una variable (undefined)
+!greaterThan5
+//expresion que retorna false
+!(num === 10)
+//expresion que retorna false
