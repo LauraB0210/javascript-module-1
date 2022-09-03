5192d3db4825cc3b0092b30c2dd2d974776a4282
Laura Beleño <vaneessa10@gmail.com>
2022-09-03 12:57:10 +0200

    ejercicio K semana dos

---
+++ week-2/InClass/exercise-K.js
@@ -0,0 +1,6 @@
+const nameClass= ["Gabriel","Natalia", "Graciela", "Linda", "Lina", "Laura", "Andrea", "Fernanda", "Luis", "Lixi","Wiston", "Oswaldo", "Karelys",
+];
+
+for (let i = 0; i < nameClass.length; i++) {
+    console.log(nameClass[i]);  
+}
