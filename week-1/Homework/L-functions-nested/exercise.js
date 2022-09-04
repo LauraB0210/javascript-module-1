723cae7bd558902b84528bf8092e9c8085a0fec3
Laura Beleño <vaneessa10@gmail.com>
2022-09-04 14:03:55 +0200

    ejercicios homewoork finalizados

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -3,3 +3,18 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function nameMayuscula (mentor){
+    const nameMayuscula = mentor.toUpperCase();
+    return nameMayuscula;
+}
+
+function saludo(mentor){
+    return "Hola " + " " + nameMayuscula (mentor)
+}
+
+console.log(saludo(mentor1));
+console.log(saludo(mentor2));
+console.log(saludo(mentor3));
+console.log(saludo(mentor4));
+console.log(saludo(mentor5));
